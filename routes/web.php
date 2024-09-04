23c7e4df82b46c8a7a8c1c6436310a856731b37f
ahmedSamirSoftwareEng <ahmedsamirsoftwareengineer@gmail.com>
2024-09-05 01:19:15 +0300

    firstcommit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+Auth::routes();
+
+// Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/{page}', [AdminController::class, 'index']);
+
+
